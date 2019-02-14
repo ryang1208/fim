fb4be5dcc44578da328474c2d3bcf712d3da22a8
RichardYang <3327959823@qq.com>
2019-02-14 10:57:26 +0800

    Initial commit

---
+++ src/main/java/com/huaxin/fim/FimApplication.java
@@ -0,0 +1,14 @@
+package com.huaxin.fim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FimApplication.class, args);
+    }
+
+}
+
